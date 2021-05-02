44a9f4f72bc994e1a98cd9c6769102a2af355575
theoellingsen <83192769+theoellingsen@users.noreply.github.com>
2021-05-03 09:51:46 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A2SDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
