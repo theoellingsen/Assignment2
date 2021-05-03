0b550de62a9fdc651287074467edbfef13e898ff
theoellingsen <83192769+theoellingsen@users.noreply.github.com>
2021-05-03 11:06:12 +1000

    Added position and researcher classes

---
+++ Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2SDD
+{
+    enum Level {A, B, C, D, E}
+    class Position
+    {
+        public Level Level { get; set };
+    }
+}
---
+++ Researcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2SDD
+{
+
+    class Researcher
+    { 
+        public int ID { get; set; }
+
+        public string givenName { get; set; }
+
+        public string familyName { get; set; }
+
+        public string title { get; set; }
+
+        public string school { get; set; }
+
+        public string campus { get; set; }
+
+        public string email { get; set; }
+
+        public string photo { get; set; }
+
+    }
+}
